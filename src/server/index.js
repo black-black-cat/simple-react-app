9f4faa7a929bdfada81b1a46ce3c546978126878
black-black-cat <k99apply@gmail.com>
2017-01-20 15:27:51 +0800

    create

---
+++ src/server/index.js
@@ -0,0 +1,5 @@
+import Dog from '../shared/Dog';
+
+const toby = new Dog('Toby');
+
+console.log(toby.bark());
---
+++ webpack.config.babel.js
@@ -0,0 +1,18 @@
+export default {
+  output: {
+    filename: 'client-bundle.js',
+  },
+  devtool: '#eval',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: [/node_modules/],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+};
